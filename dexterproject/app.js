1298b8b31f0ee0fb901fbc8a95cf12ed775c2be7
undvictor <undvictor@gmail.com>
2015-12-18 14:49:46 +0100

    new tree structure and made runnable

--- dexterproject/app.js
+++ dexterproject/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var live = require('./routes/live');
+
 
 var app = express();
 
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/live', live);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
